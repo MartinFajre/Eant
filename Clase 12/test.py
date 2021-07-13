1de8c454b36b88ba0b451b4c8f1c9c07d9bc0d6e
MartinFajre <marzxq@hotmail.com>
2021-07-13 17:27:16 -0300

    Primer modelo de rutas dinamicas

--- Clase 12/test.py
+++ Clase 12/test.py
@@ -1,26 +1,33 @@
-# Utilizamos la libreria Flask
-# El Flask tiene que estar corriendo para funcione la pag, sino queda "apagado"
-
-pip install flask
-
 from flask import Flask
-
-app = Flask(__name__)   # Toma el nombre del archivo, que en este caso es test.py
-
-@app.route('/')     # Así accedo a la pagina principal o raiz.
-@app.route('/institucional/sobre-nosotros')     #Asi accedo a una ruta específica de ese dominio
+from flask import json
 
 
-@app.route('/')
-def hello_flask():
-    return '<h1>Hola Mundo</h1>'
+app = Flask(__name__)   
 
 
+@app.route('/users')
+def twitter_Users():
+    users = [
+        {'name':'tincchofajre'},
+        {'name':'eantech'},
+        {'name':'TinchoLutter'},
+        {'name':'bitcoinArg'}
+        ]
+    
+    response = app.response_class( response = json.dumps(users), status = 200, mimetype = 'application/json' )
+    
+    return response
 
+   
 
-# De esta forma habilito que cualquier dispositivo de mi intranet pueda acceder a mi maquina utilizando la IP que identifica mi maquina
-app.run( port = 3030, host = '0.0.0.0' )    
-
- 
-
-app.run( port = 3030 )
+@app.route('/users/<path>')
+def searchUsers(path):
+    if path == 'people':
+        return 'ACA VA UN JSON DE PERSONAS'
+    elif path == 'company':
+        return 'ACA VA UN JSON DE EMPRESAS'
+    else:
+        return 'UPPS.. NO PUEDO BUSCAR LO QUE ESTAS PIDIENDO'
+    
+app.run( port = 3030, host = '0.0.0.0' )     
+    
